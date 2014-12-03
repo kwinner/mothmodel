2e4b6310aeff6d2c04d1108306a938390a4577fe
Kevin Winner <kwinner@cs.umass.edu>
2014-12-03 16:57:16 -0500

    merged the ML theta estimators

--- CIcoverage.m
+++ CIcoverage.m
@@ -1,6 +1,6 @@
 function coverage = CIcoverage(theta_0, params, varargin)
 
-DEFAULT_ITERATIONS = 2;
+DEFAULT_ITERATIONS = 10;
 
 parser = inputParser;
 
@@ -16,30 +16,40 @@ niterations = parser.Results.iterations;
 
 theta_0_mat = [theta_0.mu, theta_0.sigma, theta_0.lambda];
 
-nparams   = numel(theta_0_mat);
-coverage  = zeros(niterations, nparams);
+nparams       = numel(theta_0_mat);
+coverage_GP   = zeros(niterations, nparams);
+coverage_zonn = zeros(niterations, nparams);
 
 for iteration = 1:niterations
 	% if mod(iteration, 10) == 0
-	% 	fprintf('iteration #%d...\n', iteration);
+		fprintf('iteration #%d...\n', iteration);
 	% end
-	
+
 	%sample from the true theta
 	y_i     = sampleState(theta_0, params);
 
 	%learn theta from sample, including the CI
-	[theta_i, CI_i] = zonneveldtheta(y_i, params);
+	[theta_GP, CI_GP]     = MLtheta(y_i, params, 'theta_0', theta_0_mat, 'objective', 'GP');
+	[theta_zonn, CI_zonn] = MLtheta(y_i, params, 'theta_0', theta_0_mat, 'objective', 'zonn');
 
 	%check which CIs cover theta_0
 	for iparam = 1:nparams
-		if CI_i(1, iparam) <= theta_0_mat(iparam) && CI_i(2, iparam) >= theta_0_mat(iparam)
-			coverage(iteration, iparam) = 1;
+		if CI_GP(1, iparam) <= theta_0_mat(iparam) && CI_GP(2, iparam) >= theta_0_mat(iparam)
+			coverage_GP(iteration, iparam) = 1;
+		end
+		if CI_zonn(1, iparam) <= theta_0_mat(iparam) && CI_zonn(2, iparam) >= theta_0_mat(iparam)
+			coverage_zonn(iteration, iparam) = 1;
 		end
 	end
 end
 
 %normalize the coverage counter
-coverage = sum(coverage, 1);
-coverage = coverage ./ niterations;
+coverage_GP   = sum(coverage_GP, 1);
+coverage_GP   = coverage_GP ./ niterations;
+coverage_zonn = sum(coverage_zonn, 1);
+coverage_zonn = coverage_zonn ./ niterations;
+
+coverage.GP   = coverage_GP;
+coverage.zonn = coverage_zonn;
 
 end
--- old/abundancy.m
+++
@@ -1,9 +0,0 @@
-function [ n ] = abundancy( q )
-
-T = size(q,1) - 1;
-
-%each element of n is a 2D sum of a submatrix of q
-n = arrayfun(@(k) sum(sum(q(1:k, k+1:T+1))), 1:T);
-
-end
-
