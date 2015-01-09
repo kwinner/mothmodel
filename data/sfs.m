6f15ab38163d0565d33b1ac58fe45c96ff69b96a
kwinner <kwinner@localhost>
2015-01-09 17:23:49 -0500

    likelihood surface code

--- data/sfs.m
+++ data/sfs.m
@@ -3,7 +3,7 @@ function [ data ] = sfs ( filename )
 %   Detailed explanation goes here
 
 if ~exist('filename', 'var')
-	filename = '/Users/kwinn/Work/Data/moth/sfs2004.csv';
+	filename = '/Users/kwinner/Work/data/moth/sfs2004.csv';
 end
 
 data = csv2struct(filename, '%s%f%f%f%f');
