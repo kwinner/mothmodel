f98f7b23b0784aead512c238cf128c5bc326421e
gbernstein <gbernstein@localhost>
2015-01-11 18:56:44 -0500

    finished code refactor to plot burn in times. also added in code into mcmc to use ARS, thought it doesnt work yet. added ARS_experiment to start experimentation with it.

--- loglikelihood.m
+++ loglikelihood.m
@@ -2,9 +2,13 @@ function [ LL ] = loglikelihood (state, y, params)
 
 LL = gammaln(params.N+1) + sum(alogb(state.q(:), state.p(:))) - sum(gammaln(state.q(:)+1)) + sum(logbinopdf(y, state.n, params.alpha));
 
+% if isinf(LL) && LL < 0
+%     LL = -10^10;
+% end
+
 end
 
 function s = alogb(a, b)
-	s = a.*log(b);
-	s(a==0) = 0;
+s = a.*log(b);
+s(a==0) = 0;
 end
--- mcmc.m
+++ mcmc.m
@@ -2,31 +2,36 @@ function [ state ] = mcmc( y, state_0, params, varargin )
 
 DEFAULT_ITERATIONS = 1000;
 DEFAULT_MOVES      = {'pairwise', 'shuffle', 'mergesplit'};
+DEFAULT_USE_ARS    = false;
 
 parser = inputParser;
 addParamValue(parser, 'iterations', DEFAULT_ITERATIONS, @isnumeric);
 addParamValue(parser, 'moves',      DEFAULT_MOVES,      @iscell);
+addParamValue(parser, 'use_ARS',      DEFAULT_USE_ARS,      @islogical);
 
 parse(parser, varargin{:});
 nIterations = parser.Results.iterations;
 moves       = parser.Results.moves;
+use_ARS     = parser.Results.use_ARS;
 
 %initialize
-state = state_0;
+state(nIterations) = state_0;
+state(1) = state_0;
 
-iter = 2;
-while iter <= nIterations + 1
-	%do one sample
-	[state(iter), success] = gibbsSample(y, state(iter-1), params, 'moves', moves);
-
-	%if the sample was successful, move on
-	%unsuccessful moves are, for instance, moves which select an illegal pair to xfer
-	%or which have no available slack
-	if ~success
-		continue
-	else
-		iter = iter + 1;
-	end
+% iter = 2;
+% while iter <= nIterations + 1
+for iter = 2:nIterations
+    %do one sample
+    [state(iter), success] = gibbsSample(y, state(iter-1), params, 'moves', moves, 'use_ARS', use_ARS);
+    
+    %if the sample was successful, move on
+    %unsuccessful moves are, for instance, moves which select an illegal pair to xfer
+    %or which have no available slack
+%     if ~success
+%         continue
+%     else
+%         iter = iter + 1;
+%     end
 end
 
 end
@@ -34,12 +39,15 @@ end
 function [ state, success ] = gibbsSample( y, state_0, params, varargin )
 
 DEFAULT_MOVES = {'pairwise', 'shuffle', 'mergesplit'};
+DEFAULT_USE_ARS    = false;
 
 parser = inputParser;
 addParamValue(parser, 'moves', DEFAULT_MOVES, @iscell);
+addParamValue(parser, 'use_ARS',      DEFAULT_USE_ARS,      @islogical);
 
 parse(parser, varargin{:});
 moves = parser.Results.moves;
+use_ARS = parser.Results.use_ARS;
 
 T  = length(y);     %# of observations
 NQ = (T+1)*(T+2)/2; %size of upper triangular part of Q (# of valid outcomes)
@@ -48,146 +56,90 @@ NQ = (T+1)*(T+2)/2; %size of upper triangular part of Q (# of valid outcomes)
 move = moves{randi(numel(moves))};
 
 switch move
-	case 'pairwise'
-		%select two cells in q to rebalance
-		i = randsample(NQ, 2, false);
-		move = struct;
-		[move.a, move.b] = ind2sub_triu(T+1, i(1));
-		[move.c, move.d] = ind2sub_triu(T+1, i(2));
+    case 'pairwise'
+        %select two cells in q to rebalance
+        i = randsample(NQ, 2, false);
+        move = struct;
+        [move.a, move.b] = ind2sub_triu(T+1, i(1));
+        [move.c, move.d] = ind2sub_triu(T+1, i(2));
+        
+        %%sample delta, the # of indivs to move from q(a,b) to q(c,d)
+        
+        %the slack computations below rely on the fact that if you xfer some
+        %mass from q(a,b) to q(c,d), some observations increase, some decrease, and some see both changes
+        %nAB/nCD are the cells that change an amount equal to the change in q(a,b)/q(c,d) respectively
+        %and the corresponding slack is how far those abundancies are over the corresponding observations
+        
+        %how much slack is there in the abundancies that change with ab
+        move.nAB = union(move.a : (min(move.b,move.c) - 1), max(move.a,move.d) : (move.b - 1));
+        if isempty(move.nAB) nABSlack = Inf; %the cells that change with AB are a subset of those that change with CD
+        else
+            nABSlack = min(state_0.n(move.nAB) - y(move.nAB));
+        end
+        
+        %how much slack is there in the abundancies that change with cd
+        move.nCD = union(move.c : (min(move.d,move.a) - 1), max(move.c,move.b) : (move.d - 1));
+        if isempty(move.nCD) nCDSlack = Inf; %the cells that change with CD are a subset of those that change with AB
+        else
+            nCDSlack = min(state_0.n(move.nCD) - y(move.nCD));
+        end
+        
+        abMax = min(state_0.q(move.a,move.b), nABSlack);
+        cdMax = min(state_0.q(move.c,move.d), nCDSlack);
+        
+        %if there is no slack available, then no move can be made
+        if abMax == 0 && cdMax == 0
+            success = false; state = state_0;
+            return
+        end
+        
+        %indices of P/Q that change with delta
+        move.changedPQ = sub2ind([T+1,T+1], [move.a move.c], [move.b move.d]);
+        move.changedNY = union(move.nAB, move.nCD);
+        
+        %range of values for delta
+        %max negative is the maximum # that can be taken from ab
+        %max positive is the maximum # that can be taken from cd
+        deltaRange = [-abMax:cdMax];
+             
+        %which move we'll be applying
+        move.applydelta = @applydelta_pairwise;
 
-		%%sample delta, the # of indivs to move from q(a,b) to q(c,d)
+    case 'shuffle'
+        %note: this move is very similar to 'pairwise' above, just with *many* simplifications
+        %select two unobserved q to shuffle
+        i = randsample(T, 2, false);
+        move = struct;
+        move.a = i(1); move.b = i(1);
+        move.c = i(2); move.d = i(2);
+        
+        %since abundancy won't change w/ a shuffle, the delta limits are easy
+        deltaRange = -state_0.q(move.a, move.b):state_0.q(move.c, move.d);
+        
+        %which move we'll be applying
+        move.applydelta = @applydelta_pairwise;
+        
+    case 'mergesplit'
+        %note: this is also an abundancy-preserving move, but with more complex
+        %bound on delta
+        %since it is slightly differeny from above,
+        %positive delta in a m/s corresponds to increasing q(a,c)
+        
+        %sample three indices, i is the smallest, k is the largest
+        move = struct;
+        temp = sort(randsample(T+1, 3, false));
+        move.i = temp(1); move.j = temp(2); move.k = temp(3);
+        
+        %smaller of the two bins being merged
+        maxMerge = min(state_0.q(move.i,move.j), state_0.q(move.j,move.k));
+        %smaller of the large bin, or the unobserved slack at j,j
+        maxSplit = min(state_0.q(move.i,move.k), state_0.q(move.j,move.j));
+        
+        deltaRange = -maxSplit:maxMerge;
+        
+        %which move we'll be applying
+        move.applydelta = @applydelta_ms;     
 
-		%the slack computations below rely on the fact that if you xfer some
-		%mass from q(a,b) to q(c,d), some observations increase, some decrease, and some see both changes
-		%nAB/nCD are the cells that change an amount equal to the change in q(a,b)/q(c,d) respectively
-		%and the corresponding slack is how far those abundancies are over the corresponding observations
-
-		%how much slack is there in the abundancies that change with ab
-		move.nAB = union(move.a : (min(move.b,move.c) - 1), max(move.a,move.d) : (move.b - 1));
-		if isempty(move.nAB) nABSlack = Inf; %the cells that change with AB are a subset of those that change with CD
-		else
-			nABSlack = min(state_0.n(move.nAB) - y(move.nAB));
-		end
-
-		%how much slack is there in the abundancies that change with cd
-		move.nCD = union(move.c : (min(move.d,move.a) - 1), max(move.c,move.b) : (move.d - 1));
-		if isempty(move.nCD) nCDSlack = Inf; %the cells that change with CD are a subset of those that change with AB
-		else
-			nCDSlack = min(state_0.n(move.nCD) - y(move.nCD));
-		end
-
-		abMax = min(state_0.q(move.a,move.b), nABSlack);
-		cdMax = min(state_0.q(move.c,move.d), nCDSlack);
-
-		%if there is no slack available, then no move can be made
-		if abMax == 0 && cdMax == 0
-			success = false; state = state_0;
-			return
-		end
-
-		%indices of P/Q that change with delta
-		move.changedPQ = sub2ind([T+1,T+1], [move.a move.c], [move.b move.d]);
-		move.changedNY = union(move.nAB, move.nCD);
-
-		%range of values for delta
-		%max negative is the maximum # that can be taken from ab
-		%max positive is the maximum # that can be taken from cd
-		deltaRange = [-abMax:cdMax];
-
-		%compute the LL over all possible delta
-		move.applydelta = @applydelta_pairwise;
-		UNLL = arrayfun(@(delta) loglikelihoodofdelta(delta, state_0, y, params, move), deltaRange);
-
-		%anywhere that UNLL = -inf is impossible and should be removed
-		%this typically only occurs when params.alpha = 1 and delta makes the observations impossible
-		deltaRange = deltaRange(UNLL ~= inf);
-		UNLL = UNLL(UNLL ~= inf);
-
-		NLL = normalizeLikelihood(UNLL);
-
-		if any(NLL < 0) || all(NLL == 0)
-			success = false;
-			state = state_0;
-			return
-		end
-
-		%randomly sample delta from the normalized LL
-		delta = deltaRange(randsample(numel(deltaRange), 1, true, NLL));
-
-		state = applydelta_pairwise(delta, state_0, move);
-		success = true;
-	case 'shuffle'
-		%note: this move is very similar to 'pairwise' above, just with *many* simplifications
-		%select two unobserved q to shuffle
-		i = randsample(T, 2, false);
-		move = struct;
-		move.a = i(1); move.b = i(1);
-		move.c = i(2); move.d = i(2);
-
-		%since abundancy won't change w/ a shuffle, the delta limits are easy
-		deltaRange = -state_0.q(move.a, move.b):state_0.q(move.c, move.d);
-
-		%compute the LL over all possible delta
-		move.applydelta = @applydelta_pairwise;
-		UNLL = arrayfun(@(delta) loglikelihoodofdelta(delta, state_0, y, params, move), deltaRange);
-
-		%unlike the pairwise case, this should never be triggered, but better safe than sorry!
-		deltaRange = deltaRange(UNLL ~= inf);
-		UNLL = UNLL(UNLL ~= inf);
-
-		NLL = normalizeLikelihood(UNLL);
-
-		if any(NLL < 0) || all(NLL == 0)
-			success = false;
-			state = state_0;
-			return
-		end
-
-
-		%randomly sample delta from the normalized LL
-		delta = deltaRange(randsample(numel(deltaRange), 1, true, NLL));
-
-		state = applydelta_pairwise(delta, state_0, move);
-		success = true;
-	case 'mergesplit'
-		%note: this is also an abundancy-preserving move, but with more complex
-		%bound on delta
-		%since it is slightly differeny from above,
-		%positive delta in a m/s corresponds to increasing q(a,c)
-
-		%sample three indices, i is the smallest, k is the largest
-		move = struct;
-		temp = sort(randsample(T+1, 3, false));
-		move.i = temp(1); move.j = temp(2); move.k = temp(3);
-
-		%smaller of the two bins being merged
-		maxMerge = min(state_0.q(move.i,move.j), state_0.q(move.j,move.k));
-		%smaller of the large bin, or the unobserved slack at j,j
-		maxSplit = min(state_0.q(move.i,move.k), state_0.q(move.j,move.j));
-
-		deltaRange = -maxSplit:maxMerge;
-
-		%compute the LL over all possible delta
-		move.applydelta = @applydelta_ms;
-		UNLL = arrayfun(@(delta) loglikelihoodofdelta(delta, state_0, y, params, move), deltaRange);
-
-		%this should also never happen w/ a ms
-		deltaRange = deltaRange(UNLL ~= inf);
-		UNLL = UNLL(UNLL ~= inf);
-
-		NLL = normalizeLikelihood(UNLL);
-
-		if any(NLL < 0) || all(NLL == 0)
-			success = false;
-			state = state_0;
-			return
-		end
-
-		%randomly sample delta from the normalized LL
-		delta = deltaRange(randsample(numel(deltaRange), 1, true, NLL));
-
-		state = applydelta_ms(delta, state_0, move);
-		success = true;
     case 'cycle'
         move = struct;
         temp = sort(randsample(T+1, 4, true));
@@ -208,28 +160,42 @@ switch move
         
         deltaRange = -maxPos:maxNeg;
         
-        %compute the LL over all possible delta
+        %which move we'll be applying
         move.applydelta = @applydelta_cycle;
-        UNLL = arrayfun(@(delta) loglikelihoodofdelta(delta, state_0, y, params, move), deltaRange);
-        
-        %this shouldn't ever happen
-        deltaRange = deltaRange(UNLL ~= inf);
-        UNLL = UNLL(UNLL ~= inf);
-        
-        NLL = normalizeLikelihood(UNLL);
-        
-        if any(NLL < 0) || all(NLL == 0)
-            success = false;
-            state = state_0;
-            return
-        end
-        
-        %randomly sample delta
-        delta = deltaRange(randsample(numel(deltaRange), 1, true, NLL));
-        
-        state = applydelta_cycle(delta, state_0, move);
-        success = true;
 end
+
+%our log-likelhood function
+logp = @(delta) loglikelihoodofdelta(delta, state_0, y, params, move);
+
+if use_ARS
+    
+    bounds = [min(deltaRange),max(deltaRange)];
+    points = [];
+    nsamples = 1;
+    
+    sampled_delta = discrete_ars(logp, bounds, points, nsamples);
+else
+    %compute the LL over all possible delta
+    UNLL = arrayfun(@(delta) logp(delta), deltaRange);
+    
+    %this shouldn't ever happen
+    deltaRange = deltaRange(UNLL ~= inf);
+    UNLL = UNLL(UNLL ~= inf);
+    
+    NLL = normalizeLikelihood(UNLL);
+    
+    if any(NLL < 0) || all(NLL == 0)
+        success = false;
+        state = state_0;
+        return
+    end
+
+    %randomly sample delta
+    sampled_delta = deltaRange(randsample(numel(deltaRange), 1, true, NLL));
+end
+
+state = move.applydelta(sampled_delta, state_0, move);
+success = true;
 
 end
 
@@ -241,10 +207,10 @@ state_prime.q(move.a,move.b) = state.q(move.a,move.b) + delta;
 state_prime.q(move.c,move.d) = state.q(move.c,move.d) - delta;
 
 if isfield(move,'nAB')
-	state_prime.n(move.nAB) = state.n(move.nAB) + delta;
+    state_prime.n(move.nAB) = state.n(move.nAB) + delta;
 end
 if isfield(move,'nCD')
-	state_prime.n(move.nCD) = state.n(move.nCD) - delta;
+    state_prime.n(move.nCD) = state.n(move.nCD) - delta;
 end
 
 end
